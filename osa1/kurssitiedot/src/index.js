38003fb6e8db3af5f261a2d64dad886ab0a70798
Niilo Latva-Pukkila <niilo.latva-pukkila@kesko.fi>
2019-12-30 15:47:07 +0200

    use lists

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -35,7 +35,7 @@ const Total = (props) => {
     return (
         <div>
             <p>
-                Number of exercises {props.num1 + props.num2 + props.num3}
+                Number of exercises {props.parts[0]['exercises'] + props.parts[1]['exercises'] + props.parts[2]['exercises']}
             </p>
         </div>
     )
@@ -43,25 +43,27 @@ const Total = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-      }
-      const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-      }
-      const part3 = {
-        name: 'State of a component',
-        exercises: 14
-      }
-    
+    const parts = [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+        
 
   return (
     <div>
       <Header title={course}/>
-      <Content parts={[part1, part2, part3]}/>
-      <Total num1={part1['exercises']} num2={part2['exercises']} num3={part3['exercises']} />
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
