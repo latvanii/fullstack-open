34506b3dad73ed2f699efa8782093d6c8243f042
Niilo Latva-Pukkila <niilo.latva-pukkila@kesko.fi>
2020-01-03 15:25:15 +0200

    step5

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -13,8 +13,31 @@ const Button = (props) => (
     </button>
   )  
 
+const Statistic = ({text, value}) => {
+    return (<div>{text}: <Display counter={value}/></div>)
+}
+
+const Statistics = ({bad, good, neutral}) => {
+    const total = bad + neutral + good
+    if (total === 0) {
+        return (
+            <div>No feedback given</div>
+        )
+    }
+    return (
+        <div>
+            <Statistic text="good" value = {good} />
+            <Statistic text="neutral" value = {neutral} />
+            <Statistic text="bad" value = {bad} />
+            <Statistic text="all" value = {total} />
+            <Statistic text="average" value = {(good-bad) / total} />
+            <Statistic text="positive" value = {good/ total} />
+        </div>
+    )
+
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -31,12 +54,6 @@ const App = () => {
     setBad(bad + 1)
   }
 
-
-  const [value, setValue] = useState(10)
-  const setToValue = (newValue) => () => {
-    setValue(newValue)
-  }
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -44,9 +61,7 @@ const App = () => {
       <Button handleClick={() => increaseNeutral()} text="neutral" />
       <Button handleClick={() => increaseBad()} text="bad" />
       <h1>statistics</h1>
-      <div>good: <Display counter={good}/></div>
-      <div>neutral: <Display counter={neutral}/></div>
-      <div>bad: <Display counter={bad}/></div>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
