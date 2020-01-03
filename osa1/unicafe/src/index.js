a40b353a1f052ad683904ac48811a58d52d361bb
Niilo Latva-Pukkila <niilo.latva-pukkila@kesko.fi>
2020-01-03 14:48:46 +0200

    step1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,15 +1,52 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Display = (props) => {
+    return (
+      props.counter
+    )
+  }
+  
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )  
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const increaseGood = () => {
+    setGood(good + 1)
+  }
+
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const increaseBad = () => {
+    setBad(bad + 1)
+  }
+
+
+  const [value, setValue] = useState(10)
+  const setToValue = (newValue) => () => {
+    setValue(newValue)
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button handleClick={() => increaseGood()} text="good" />
+      <Button handleClick={() => increaseNeutral()} text="neutral" />
+      <Button handleClick={() => increaseBad()} text="bad" />
+      <h1>statistics</h1>
+      <div>good: <Display counter={good}/></div>
+      <div>neutral: <Display counter={neutral}/></div>
+      <div>bad: <Display counter={bad}/></div>
     </div>
   )
 }
