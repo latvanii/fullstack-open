236ada0f34248f5336e604210f2074b18446eeb6
ADM Niilo Latva-Pukkila <adm-niilo@ADM-MBP-2.home>
2020-01-06 10:48:33 +0200

    refactor kurssitiedot

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -11,22 +11,21 @@ const Header = (props) => {
     )
 }
 
-const Part = (props) => {
+const Part = ({part}) => {
     return (
-        <div>
-            <p>
-                {props.part['name']} {props.part['exercises']}
-            </p>
-        </div>
+        <li>
+            {part['name']} {part['exercises']}
+        </li>
     )
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
+    const format_parts = () => parts.map(part =>
+        <Part part={part} key={part.id}/>
+        )
     return (
         <div>
-            <Part part={props.parts[0]}/>
-            <Part part={props.parts[1]}/>
-            <Part part={props.parts[2]}/>
+            {format_parts()}
         </div>
     )
 }
@@ -41,33 +40,49 @@ const Total = (props) => {
     )
 }
 
+const Course = ({course}) => {
+    return (
+        <div>
+            <Header title={course['name']}/>
+            <Content parts={course['parts']}/>
+            {/* <Total parts={course['parts']}/> */}
+        </div>
+    )
+}
+
 const App = () => {
     const course = {
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7
-          },
-          {
-            name: 'State of a component',
-            exercises: 14
-          }
-        ]
-      }
-            
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Fourth course',
+          exercises: 8,
+          id: 4
+        }
+      ]
+    }
+  
+    return (
+      <div>
+        <Course course={course} />
+      </div>
+    )
+  }
 
-  return (
-    <div>
-      <Header title={course['name']}/>
-      <Content parts={course['parts']}/>
-      <Total parts={course['parts']}/>
-    </div>
-  )
-}
 
 ReactDOM.render(<App />, document.getElementById('root'))
